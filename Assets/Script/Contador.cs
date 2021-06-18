4f7d4a50447edaafe55b5bce4139fbd0382d597c
Vitor Batista <42703631+shadomal@users.noreply.github.com>
2021-06-17 21:03:41 -0300

    Insert New player Object and finish mechanics turn
    Finalizado a mudança de turno automatica, e novo player com arma colocado em cena.

---
+++ Assets/Script/Contador.cs
@@ -0,0 +1,20 @@
+﻿using System.Net.Mime;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Contador : MonoBehaviour
+{
+    public void TurnCount(bool isReady, float timer, Text textTimer)
+    {
+        if (isReady)
+        {
+            timer -= Time.deltaTime;
+            textTimer.text = timer.ToString("0");
+            if (timer <= 0)
+            {
+                isReady = false;
+            }
+        }
+    }
+}
---
+++ Assets/Script/PlayersFolder/Gun/DrawProjection.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawProjection : MonoBehaviour
+{
+    Shoot shoot;
+    LineRenderer lineRenderer;
+
+   //Number of points on the line
+    public int numPoints = 50;
+
+    //Distance between those points on the line
+    public float timeBetweenPoints = 0.1f;
+
+    //Thephysics layers that will cause the line to stop being drawn
+    public LayerMask CollidableLayers;
+
+    void Start()
+    {
+        shoot = GetComponent<Shoot>();
+        lineRenderer = GetComponent<LineRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        lineRenderer.positionCount = numPoints;
+        List<Vector3> points = new List<Vector3>();
+        Vector3 startingPosition = shoot.ShotPoint.position;
+        Vector3 startingVelocity = shoot.ShotPoint.up * shoot.BlastPower;
+        for(float t = 0; t < numPoints; t += timeBetweenPoints)
+        {            
+            Vector3 newPoint = startingPosition + t * startingVelocity;
+            newPoint.y = startingPosition.y + startingVelocity.y * t + Physics.gravity.y / 2f * t * t;
+            points.Add(newPoint);
+
+            if(Physics.OverlapSphere(newPoint, 2, CollidableLayers).Length > 0)
+            {
+                lineRenderer.positionCount = points.Count;
+                break;                
+            }            
+        }
+
+        lineRenderer.SetPositions(points.ToArray());        
+    }
+}
